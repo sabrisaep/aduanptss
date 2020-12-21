cc38b9abcb94e21406610e1a31d03f294f066480
sabrisaep <sabrisaep@gmail.com>
2020-12-21 13:53:54 +0800

    baiki coding graf

--- graf/conn.php
+++ graf/conn.php
@@ -1,2 +1,3 @@
 <?php
-$conn = new mysqli('localhost', 'root', '123456', 'apps_aduanptss');
+#$conn = new mysqli('localhost', 'root', '123456', 'apps_aduanptss');
+$conn = new mysqli('10.32.1.165', 'root', '01000kangar', 'apps_aduanptss');
